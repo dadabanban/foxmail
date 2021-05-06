2cf56f82b29cce2f8fa28b526de2422070c13351
dabanban <chenyue@nes.com.cn>
2021-05-06 13:53:52 +0800

    cc

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * * Created by malaohu on 2020/05/10.
 */
 
-//黑名单前缀...?test
+//黑名单前缀...?test1
 let pre_blacklist = ["admin","master", "info", "mail", "webadmin", "webmaster", "noreply", "system", "postmaster"]
 
 module.exports = {pre_blacklist:pre_blacklist}
